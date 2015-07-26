b5060445ed6e09238df1f8c07d30710851db1883
Jan Melcher <mail@jan-melcher.de>
2015-07-26 14:09:37 +0200

    Set a few more window rules

--- config.h
+++ config.h
@@ -28,9 +28,12 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            True,        -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       False,       -1 },
+	/* class             instance    title       tags mask     isfloating   monitor */
+	{ "Gimp",            NULL,       NULL,       0,            True,        -1 },
+	{ "Firefox",         NULL,       NULL,       1 << 8,       False,       1 },
+	{ "Thunderbird",     NULL,       NULL,       1 << 0,       False,       0 },
+	{ "Skype",           NULL,       NULL,       1 << 0,       False,       0 },
+	{ "VirtualBox",      NULL,       NULL,       1 << 1,       False,       0 },
 };
 
 /* layout(s) */
